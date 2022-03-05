7c2c3fdc9386b580333412d3f193004a80b1b589
Lavinia Matilda <lavimati@Mini-di-Lavinia.homenet.telecomitalia.it>
2022-03-05 15:39:14 +0100

    Add gulpfile.js with compileIndex and copy js resources

---
+++ gulpfile.js/jsTasks.js
@@ -0,0 +1,15 @@
+const gulp = require("gulp");
+
+const copyJs = function() {
+
+    const jsIndex = "./src/js/index.js";
+    const utilsIndex = "./src/js/utils.js";
+    const modelsIndex = "./src/js/models/Wallet.js";
+    return gulp.src([jsIndex, utilsIndex, modelsIndex], { base: "./src"})
+        .pipe(gulp.dest("./dist"));
+
+};
+
+module.exports = {
+    copyJs: copyJs
+}
