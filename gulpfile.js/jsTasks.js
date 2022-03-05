4dbd44f93d0aa50cbc3c52c47c6b6b3e798d392a
Lavinia Matilda <lavimati@Mini-di-Lavinia.homenet.telecomitalia.it>
2022-03-05 16:53:42 +0100

    Add paths.js

--- gulpfile.js/jsTasks.js
+++ gulpfile.js/jsTasks.js
@@ -1,16 +1,17 @@
 const gulp = require("gulp");
+const paths = require("./paths");
 
 const copyJs = function() {
 
-    const jsIndex = "./src/js/index.js";
-    const utilsIndex = "./src/js/utils.js";
-    const modelsIndex = "./src/js/models/Wallet.js";
-    return gulp.src([jsIndex, utilsIndex, modelsIndex], { base: "./src"})
-        .pipe(gulp.dest("./dist"));
+    const jsIndex = paths.getJsEntryPath();
+    const utilsIndex = paths.getJsEntryPath("/utils.js");
+    const modelsIndex = paths.getJsEntryPath("/models/Wallet.js");
+    return gulp.src([jsIndex, utilsIndex, modelsIndex], { base: paths.getSrcFolder()})
+        .pipe(gulp.dest(paths.getDistFolder()));
 
 };
 const watchJS = function(cb) {
-    gulp.watch("./src/js/**/*", copyJs);
+    gulp.watch(paths.getJsEntryPath("/**/*"), copyJs);
     cb();
 };
 module.exports = {
