0c2a492df212657cc3052ccac385472ab4b99509
Lavinia Matilda <lavimati@Mini-di-Lavinia.homenet.telecomitalia.it>
2022-03-06 17:53:36 +0100

    Add debug modewith sourcemaps

--- gulpfile.js/serveTasks.js
+++ gulpfile.js/serveTasks.js
@@ -3,7 +3,8 @@ const browserSync = require ("browser-sync").create();
 const paths = require("./paths");
 const serve = function(cb) {
     const prod = args.prod;
-    if(prod) {
+    const debug = args.debug;
+    if(prod && !debug) {
         return cb();
     }
     browserSync.init({
